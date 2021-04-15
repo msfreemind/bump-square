da6cae22b21e7b55de28f454572ec56a7b2523fe
Mark Swan <ms.freemind@gmail.com>
2021-04-15 14:45:30 -0600

    Added css variable to set heigh to browser area width excluding address bar

--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@ $(function () {
   const canvas = document.getElementById("myCanvas");
   const ctx = canvas.getContext("2d");
 
+  // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+  let vh = window.innerHeight * 0.01;
+  // Then we set the value in the --vh custom property to the root of the document
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+
   if (detectMobile()) {
     $('.softkey-container-1').css("display", "flex");
     $('.softkey-container-2').css("display", "flex");
